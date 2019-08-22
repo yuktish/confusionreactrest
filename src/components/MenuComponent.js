8b9ccfea5ceb3cbd1082b175de63fc60050024e5
yuktish <syukti976@gmail.com>
2019-08-22 22:54:12 +0530

    Functional Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,53 +1,36 @@
 import React from 'react';
-//import { Media } from 'reactstrap';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle } from 'reactstrap';
 
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card
+                onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
+    }
 
-function RenderMenuItem({dish, onClick}){
-  return(
-    <Card key={dish.id} onClick={() => onClick(dish.id)}>
-      <CardImg width="100%" src={dish.image} alt={dish.name} />
-        <CardImgOverlay>
-          <CardTitle>{dish.name}</CardTitle>
-        </CardImgOverlay>
-    </Card>
-  );
-}
+    const Menu = (props) => {
 
-const Menu=(props)=>{
-  const menu = props.dishes.map((dish) => {
-    return (
-      /*using media
-      <div key={dish.id} className="col-12 mt-5">
-        <Media tag="li">
-          <Media left middle>
-              <Media object src={dish.image} alt={dish.name} />
-          </Media>
-          <Media body className="ml-5">
-            <Media heading>{dish.name}</Media>
-            <p>{dish.description}</p>
-          </Media>
-        </Media>
-      </div>*/
-      <div  className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 m-1">
-        <RenderMenuItem dish={dish} onClick={props.onClick}/>
-      </div>
-    );
-});
+        const menu = props.dishes.map((dish) => {
+            return (
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
+            );
+        });
 
-return (
-  <div className="container">
-    <div className="row">
-        {menu}
-        
-    </div>
-    
-  </div>
-);
-} 
+        return (
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
+    }
 
-    
-
-
-  
 export default Menu;
